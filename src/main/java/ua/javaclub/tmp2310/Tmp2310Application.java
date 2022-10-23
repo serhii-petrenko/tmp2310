dc5fd0802ad2ab2e49486a0e408376effc8651e7
Serhii Petrenko <petrenko.zd@gmail.com>
2022-10-23 17:42:46 +0300

    init commit

---
+++ src/main/java/ua/javaclub/tmp2310/Tmp2310Application.java
@@ -0,0 +1,13 @@
+package ua.javaclub.tmp2310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tmp2310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tmp2310Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/javaclub/tmp2310/Tmp2310ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.javaclub.tmp2310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tmp2310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

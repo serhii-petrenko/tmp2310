e3058be91d3524b78ca83bda54cc5263567a0ad9
Serhii Petrenko <petrenko.zd@gmail.com>
2022-10-23 18:04:24 +0300

    add diff

--- src/main/java/ua/javaclub/tmp2310/CalculatorService.java
+++ src/main/java/ua/javaclub/tmp2310/CalculatorService.java
@@ -5,4 +5,7 @@ public class CalculatorService {
     public Integer sum(Integer a, Integer b) {
         return a + b;
     }
+    public Integer diff(Integer a, Integer b) {
+        return a - b;
+    }
 }
